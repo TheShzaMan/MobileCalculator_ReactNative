2f13eb2f8120caf36284dde75797f8f0dd8c17b8
Shea Raymond <shea.raymond01@gmail.com>
2024-02-06 00:34:28 -0600

    Setting up components with imports, style sheets and basic declarations and exports

--- MobileCalculator/components/Buttons.js
+++ MobileCalculator/components/Buttons.js
@@ -1,2 +1,16 @@
 import React from "react";
+import { Text, StyleSheet } from "react-native";
+
 import RoundButton from "./customButton";
+
+const Buttons = ({}) => {
+	return (
+		<>
+			<Text>Buttons Component</Text>;
+		</>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default Buttons;
